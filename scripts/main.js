7f2a75d5ed94c37cd616c24fc260b8f1eabd75d5
drtierney <d.r.tierney@hotmail.co.uk>
2021-03-06 16:57:13 +0000

    added js function to change the grid color on mousehover

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-//TODO Create a webpage with a 16x16 grid of square divs using js
-
-let grid = document.getElementById('grid');
-grid.style.border = "solid black 2px";
-grid.style.height = "500px";
-grid.style.width =  "1000px";
-grid.style.margin = "auto";
---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document.getElementById("grid").addEventListener(
+    "mouseover", function () {
+        document.getElementById("grid").style.background = "black";
+    }
+)
+document.getElementById("grid").addEventListener(
+    "mouseout", function () {
+        document.getElementById("grid").style.background = "white";
+    }
+)
