8ca1c233eca615bbb747b87d95f39aa9abbcf776
drtierney <d.r.tierney@hotmail.co.uk>
2021-03-06 17:47:13 +0000

    drawing the 16x16 grids

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById("grid").addEventListener(
     "mouseover", function () {
         document.getElementById("grid").style.background = "black";
@@ -8,3 +9,16 @@ document.getElementById("grid").addEventListener(
         document.getElementById("grid").style.background = "white";
     }
 )
+*/
+
+const gridContainer = document.getElementById("grid-container");
+let size = 16;
+
+for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.style.border = "1px solid black";
+    gridElement.style.background = "white";
+    gridContainer.appendChild(gridElement);
+}
+
+gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
